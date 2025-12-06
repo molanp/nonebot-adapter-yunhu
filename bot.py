f2575d988b591e8ae789118786813257eed86289
molanp <molanp>
2025-12-06 18:45:32 +0800

    更新示例配置信息

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
 import nonebot
 
-# from nonebot.adapters.discord import Adapter as DiscordAdapter
-# from nonebot.adapters.dodo import Adapter as DoDoAdapter
-# from nonebot.adapters.kaiheila import Adapter as KaiheilaAdapter
 from nonebot.adapters.yunhu import Adapter
 
 nonebot.init()
@@ -10,9 +7,6 @@ nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
-# driver.register_adapter(KaiheilaAdapter)
-# driver.register_adapter(DoDoAdapter)
-# driver.register_adapter(DiscordAdapter)
 
 nonebot.load_builtin_plugins("echo")
 
